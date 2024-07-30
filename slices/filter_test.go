3d51faed79dc62a1e6862742d86f44f9d0a724ea
Alex Story <astory11@yahoo.com>
2024-07-29 21:03:15 -0500

    more tests

--- slices/filter_test.go
+++ slices/filter_test.go
@@ -6,12 +6,22 @@ import (
 )
 
 func TestFilter(t *testing.T) {
-	// Test: Filter([]int{1, 2, 3, 4, 5}, func(v int) bool { return v%2 == 0 }) == []int{2, 4}
-	{
-		got := Filter([]int{1, 2, 3, 4, 5}, func(v int) bool { return v%2 == 0 })
-		want := []int{2, 4}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Filter test failed, got = %v; want %v", got, want)
+	t.Run("Filter works", func(t *testing.T) {
+		{
+			got := Filter([]int{1, 2, 3, 4, 5}, func(v int) bool { return v%2 == 0 })
+			want := []int{2, 4}
+			if !reflect.DeepEqual(got, want) {
+				t.Errorf("Filter test failed, got = %v; want %v", got, want)
+			}
 		}
-	}
+	})
+
+	t.Run("Filter doesnt mutate the original slice", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 5}
+		Filter(slice, func(v int) bool { return v%2 == 0 })
+		want := []int{1, 2, 3, 4, 5}
+		if !reflect.DeepEqual(slice, want) {
+			t.Errorf("Filter test failed, got = %v; want %v", slice, want)
+		}
+	})
 }
